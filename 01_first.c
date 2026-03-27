c170d1f1993e0bec3be7514e8c42ea01fe406cdf
shreeshtripathi <shreeshtripathi3@gmail.com>
2026-03-27 23:52:06 +0530

    initial commit

---
+++ 01_first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Hello world!\n");
+    printf("Took me an unhealthy amount of time to set up this IDE 😭\n");
+    printf("my first program");
+    return 0;
+}
