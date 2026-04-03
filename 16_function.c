d515d302e2359235cc5f23f0b95c7395351d82fb
shreeshtripathi <shreeshtripathi3@gmail.com>
2026-04-03 22:54:38 +0530

    add functions

--- 16_function.c
+++ 16_function.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
 
-void birthday_song() {
+void birthday_song(char name[], int age) {
     printf("Happy birthday to you!\n");
     printf("Happy birthday to you!\n");
-    printf("Happy birthday dear [name]!\n");
+    printf("Happy birthday dear %s!\n", name);
     printf("Happy birthday to you!\n");
+    printf("You are %d years old now!\n", age);
     printf("\n");
 }
 int main(){
 
-    birthday_song();
-    birthday_song();
-    birthday_song();
+    char name[100] = "";
+    int age = 0;
+
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) - 1] = '\0'; 
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    birthday_song(name,age);
+    birthday_song(name,age);
+    birthday_song(name,age);
     return 0;
 }
