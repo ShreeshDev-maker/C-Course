54fc79bb445a910b8e92cec3252bb5685bb2a000
shreeshtripathi <shreeshtripathi3@gmail.com>
2026-04-03 12:21:11 +0530

    add function

---
+++ 16_function.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+void birthday_song() {
+    printf("Happy birthday to you!\n");
+    printf("Happy birthday to you!\n");
+    printf("Happy birthday dear [name]!\n");
+    printf("Happy birthday to you!\n");
+    printf("\n");
+}
+int main(){
+
+    birthday_song();
+    birthday_song();
+    birthday_song();
+    return 0;
+}
